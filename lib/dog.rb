c6b53a2101ddb08775c41016c2da709c669b391d
Indira Kissoondyal <indirak@alumni.princeton.edu>
2018-07-25 08:57:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,16 +6,16 @@ def name
   @name
 end
 
-def name= 
-  @name   
+def name= (new_name)
+  @name = new_name 
 end
   
 def breed 
   @breed
 end 
 
-def breed=
-  @breed
+def breed= (new_breed)
+  @breed = new_breed
 end 
   
 end 
